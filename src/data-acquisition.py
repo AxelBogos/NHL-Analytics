1c7e855ce2cd7ec6607c14e8214a1abbf59d7a16
AxelBogos <35436065+AxelBogos@users.noreply.github.com>
2021-09-18 13:27:50 -0400

    Added playoffs handling

--- src/data-acquisition.py
+++ src/data-acquisition.py
@@ -9,7 +9,7 @@ class HockeyDataLoader:
 
 	def __init__(self, season_years=None):
 		if season_years is None:
-			season_years = ['2017', '2018', '2019', '2020']
+			season_years = ['2020']
 		self.SEASONS = season_years
 
 	def get_season_data(self, year: str, base_save_file_path: str = '../data/raw/') -> None:
@@ -24,20 +24,33 @@ class HockeyDataLoader:
 		assert (len(year) == 4)
 		assert (2016 <= int(year) <= 2021)
 
-		# Form Game IDs
+		# Regular Season game-ids
 		game_numbers = ["%04d" % x for x in range(1, 1272)]  # 0001, 0002, .... 1271
 		regular_season = [f'{year}02{game_number}' for game_number in game_numbers]
 
-		# Get games data
-		for game_id in regular_season:
-			self.get_game_data(game_id, year, base_save_file_path, make_asserts=False)
+		# Playoffs game-ids. eights of final
+		playoffs = [f"{year}0301{matchup}{game_number}" for matchup in range(1, 9) for game_number in range(1, 8)]
+		# quarter final
+		playoffs.extend([f"{year}0302{matchup}{game_number}" for matchup in range(1, 5) for game_number in range(1, 8)])
+		# half finals
+		playoffs.extend([f"{year}0303{matchup}{game_number}" for matchup in range(1, 3) for game_number in range(1, 8)])
+		# final
+		playoffs.extend([f"{year}0304{1}{game_number}" for game_number in range(1, 8)])
 
-	def get_game_data(self, game_id: str, year: str, base_save_file_path: str, make_asserts: bool = True) -> None:
+		# Get game data
+		for game_id in regular_season:
+			self.get_game_data(game_id, year, base_save_file_path, is_playoffs=False, make_asserts=False)
+		for game_id in playoffs:
+			self.get_game_data(game_id, year, base_save_file_path, is_playoffs=True, make_asserts=False)
+
+	def get_game_data(self, game_id: str, year: str, base_save_file_path: str, is_playoffs: bool,
+	                  make_asserts: bool = True) -> None:
 		"""
 		Get a single game data and save it to base_save_file_path/game_id.json
 		:param game_id: id of the game. ex: 2017020001
 		:param year: 4-digit desired season year. For example, '2017' for the 2017-2018 season.
 		:param base_save_file_path: Basic path from which season data dirs are created. By default, '../data/raw/'
+		:param is_playoffs: bool indicating if the game is part of the playoffs or not. Determines saving path.
 		:param make_asserts: boolean to determine whether or not make sanity checks. False if function is called from
 		get_season_data
 		:return: None
@@ -47,9 +60,16 @@ class HockeyDataLoader:
 			assert (len(year) == 4)
 			assert (2016 <= int(year) <= 2021)
 
-		file_path = os.path.join(base_save_file_path, f'{game_id}.json')
+		if is_playoffs:
+			dir_path = os.path.join(base_save_file_path, 'playoffs')
+		else:
+			dir_path = os.path.join(base_save_file_path, 'regular_season')
+
+		if not os.path.isdir(dir_path):
+			os.mkdir(dir_path)
 
 		# Check if file exists already
+		file_path = os.path.join(dir_path, f'{game_id}.json')
 		if os.path.isfile(file_path):
 			return
 
