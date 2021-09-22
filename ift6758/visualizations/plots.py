d78dfa122e9ffd2dd13547a43322bdd8000c4032
AxelBogos <35436065+AxelBogos@users.noreply.github.com>
2021-09-22 18:39:02 -0400

    Added 3rd plot function. Work in progress.

--- ift6758/visualizations/plots.py
+++ ift6758/visualizations/plots.py
@@ -56,7 +56,7 @@ class HockeyPlotter:
 			fig.savefig(os.path.join(SAVE_FIG_PATH, 'shot_type_histogram.png'))
 		return fig
 
-	def distance_vs_goal_chance(self, save_fig = True) -> plt.Figure:
+	def distance_vs_goal_chance(self, save_fig=True) -> plt.Figure:
 		"""
 		Plots a comparative graph across seasons (2017 - 2020) of the relationship between
 		shot distance and goals (as described in Part 5 Q2)
@@ -68,6 +68,7 @@ class HockeyPlotter:
 		filtered_df = self.df[self.df['shot_distance'].notnull()]
 		filtered_df['is_goal'].astype(bool)
 		fig = plt.figure(figsize=(15, 20))
+		# iterate over years to create subplots
 		for year_index, year in enumerate(['2018', '2019', '2020']):
 			plt.subplot(3, 1, year_index + 1)
 			ax = sns.histplot(
@@ -90,8 +91,31 @@ class HockeyPlotter:
 			fig.savefig(os.path.join(SAVE_FIG_PATH, "distance_vs_goal_chance.png"))
 		return fig
 
+	def distance_and_type_vs_goal(self, save_fig=True) -> plt.Figure:
+		"""
+		Depicts the relationship between shot-distance, shot_type and number of goals.
+		As described in part 5 question 3
+		TODO Work in progress.
+		:param save_fig: boolean to save the plot to SAVE_FIG_PATH
+		:return: a plt.Figure object instance
+		"""
+		fig = plt.figure(figsize= self.fig_size)
+		ax = sns.violinplot(x="shot_type",
+		               y="shot_distance",
+		               data=self.df,
+		               hue='is_goal',
+		               order=self.df[self.df['is_goal'] == True]['shot_type'].value_counts().index)
+		plt.xticks(rotation=20)
+		ax.yaxis.grid(color='gray', linestyle='dashed')
+		plt.title('Types of shot ordered by number of goals')
+		if save_fig:
+			fig.savefig(os.path.join(SAVE_FIG_PATH, "distance_and_type_vs_goal.png"))
+		plt.show()
+		return fig
+
 
 if __name__ == "__main__":
 	hockey_plotter = HockeyPlotter()
 	hockey_plotter.shot_type_histogram()
 	hockey_plotter.distance_vs_goal_chance()
+	hockey_plotter.distance_and_type_vs_goal()
