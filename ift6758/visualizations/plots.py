44149e3d48ef307ec024cae7327217ff7731800e
AxelBogos <35436065+AxelBogos@users.noreply.github.com>
2021-10-13 16:13:38 -0400

    Finalized Q5 plots 1,2 3. Saved with more indicative file names

--- ift6758/visualizations/plots.py
+++ ift6758/visualizations/plots.py
@@ -9,136 +9,132 @@ SAVE_FIG_PATH = os.path.join('..', '..', 'figures')
 
 
 class HockeyPlotter:
-	def __init__(self, fig_size=(15, 10), data_path=TIDY_DATA_PATH):
+	def __init__(self, fig_size=(15, 10)):
 		self.fig_size = fig_size
-		self.df = pd.read_csv(data_path)
 
-	def shot_type_histogram(self, save_fig=True) -> plt.Figure:
+	def shot_type_histogram(self, df, season: str = '20192020', save_fig: bool = True) -> plt.Figure:
 		"""
 		Displays a shot-type histogram as described in Part 5 Question 1
+		:param df: tidy pandas.DataFrame
+		:param season: string representation of a season, long-formatted (ex: 20192020 for the 2019-2020 season).
 		:param save_fig: boolean to save the plot to SAVE_FIG_PATH
 		:return: a plt.Figure object instance
 		"""
+		# Filter dataframe by the season of interest
+		df = df[df['season'].astype(str) == season]
 		fig = plt.figure(figsize=self.fig_size)
+
+		# All shots count plot
 		ax1 = sns.countplot(
 			x='shot_type',
-			data=self.df,
-			order=self.df['shot_type'].value_counts().index,
+			data=df,
+			order=df['shot_type'].value_counts().index,
 			palette=['#7FB5D5'],
 			label='shots'
 		)
 
-		goal_percentage = self.df[self.df['is_goal'] == True]['shot_type'].value_counts() / self.df[
-			'shot_type'].value_counts()
+		# Add percentage of shots that are goals per type of shot
+		goal_percentage = df[df['is_goal'] == True]['shot_type'].value_counts() / df['shot_type'].value_counts()
 		for idx, p in enumerate(ax1.patches):
 			height = p.get_height()
-			ax1.text(p.get_x() + p.get_width() / 2., height + 450, f'{goal_percentage[idx] * 100 : .2f}%',
-			         size=12, ha="center")
+			ax1.text(p.get_x() + p.get_width() / 2., height + 450, f'{goal_percentage[idx] * 100 : .2f}%', size=12,
+			         ha="center")
 
+		# Goal count plot
 		ax2 = sns.countplot(
 			x='shot_type',
-			data=self.df[self.df['is_goal'] == True],
-			order=self.df[self.df['is_goal'] == True]['shot_type'].value_counts().index,
+			data=df[df['is_goal'] == True],
+			order=df[df['is_goal'] == True]['shot_type'].value_counts().index,
 			palette=['#FFBC7C'],
 			label='goals'
 		)
+
+		# Format the plot
 		plt.legend(labels=['Shots', 'Goals'])
 		plt.xticks(rotation=20)
 		plt.ylabel('Count of shots')
 		plt.xlabel('Type of shot')
-		plt.title('Shot & Goal Count Per Type of Shot and percent of successful goals')
+		plt.title(
+			f'Shot & Goal Count Per Type of Shot and percent of successful goals \n {season[0:4]} - {season[4::]} season')
 		ax1.set_axisbelow(True)
 		ax1.yaxis.grid(color='gray', linestyle='dashed')
 		ax2.set_axisbelow(True)
 		ax2.yaxis.grid(color='gray', linestyle='dashed')
 		plt.show()
 		if save_fig:
-			fig.savefig(os.path.join(SAVE_FIG_PATH, 'shot_type_histogram.png'))
+			fig.savefig(os.path.join(SAVE_FIG_PATH, 'Q5-1_shot_type_histogram.png'))
 		return fig
 
-	def distance_vs_goal_chance(self, save_fig=True) -> plt.Figure:
+	def distance_vs_goal_chance(self, df, save_fig=True) -> plt.Figure:
 		"""
 		Plots a comparative graph across seasons (2017 - 2020) of the relationship between
 		shot distance and goals (as described in Part 5 Q2)
+		:param df: tidy pandas.DataFrame
 		:param save_fig: boolean to save the plot to SAVE_FIG_PATH
 		:return: a plt.Figure object instance
 		"""
 
-		self.df['season'] = self.df['game_id'].astype(str).str[0:4]
-		filtered_df = self.df[self.df['shot_distance'].notnull()]
-		filtered_df['is_goal'].astype(bool)
+		filtered_df = df[df['shot_distance'].notnull()]
+		filtered_df['shot_distance'] = filtered_df['shot_distance'].round(0)
+		filtered_df = filtered_df.groupby(["shot_distance", "season"])["is_goal"].mean().to_frame().reset_index()
 		fig = plt.figure(figsize=(15, 20))
 		# iterate over years to create subplots
-		for year_index, year in enumerate(['2018', '2019', '2020']):
+		for year_index, year in enumerate(['20182019', '20192020', '20202021']):
 			plt.subplot(3, 1, year_index + 1)
-			ax = sns.histplot(
-				data=filtered_df[filtered_df['season'] == year],
-				x='shot_distance',
-				hue='is_goal',
-				hue_order=[False, True],
-				stat='probability',
-				kde=True,
-				multiple='stack')
-			plt.title(year)
+
+			ax = sns.lineplot(x='shot_distance',y='is_goal', data=filtered_df)
+			plt.title(f'{year[0:4]} - {year[4::]} season')
 			ax.set_axisbelow(True)
 			ax.yaxis.grid(color='gray', linestyle='dashed')
-			plt.xticks(np.arange(0, 110, 10), rotation=20)
-			plt.legend(['Goal', 'Shot'])
+			plt.xticks(np.arange(0, 210, 10), rotation=20)
 			plt.xlabel('Shot distance (ft)')
-		plt.suptitle('Shot Distance vs Probability of Scoring 2018-2020 seasons')
+			plt.ylabel('Goal Probability')
+		plt.suptitle('Shot Distance vs Goal Probability (95% CI) \n 2018-19, 2019-20, 2020-21 seasons', size=14,y=0.935)
 		plt.show()
 		if save_fig:
-			fig.savefig(os.path.join(SAVE_FIG_PATH, "distance_vs_goal_chance.png"))
+			fig.savefig(os.path.join(SAVE_FIG_PATH, "Q5-2_distance_vs_goal_chance.png"))
 		return fig
 
-	def distance_and_type_vs_goal(self, save_fig=True) -> plt.Figure:
+	def distance_and_type_vs_goal(self, df, season: str = '20172018', save_fig=True) -> plt.Figure:
 		"""
 		Depicts the relationship between shot-distance, shot_type and number of goals.
 		As described in part 5 question 3
-		TODO Work in progress.
+		:param df: tidy pandas.DataFrame
+		:param season: string representation of a season, long-formatted (ex: 20192020 for the 2019-2020 season).
 		:param save_fig: boolean to save the plot to SAVE_FIG_PATH
 		:return: a plt.Figure object instance
 		"""
-		fig = plt.figure(figsize=self.fig_size)
-		ax = sns.violinplot(x="shot_type",
-		                    y="shot_distance",
-		                    data=self.df,
-		                    hue='is_goal',
-		                    order=self.df[self.df['is_goal'] == True]['shot_type'].value_counts().index)
-		plt.xticks(rotation=20)
-		ax.yaxis.grid(color='gray', linestyle='dashed')
-		plt.title('Types of shot ordered by number of goals')
-		if save_fig:
-			fig.savefig(os.path.join(SAVE_FIG_PATH, "distance_and_type_vs_goal.png"))
+		filtered_data = df.dropna(subset=["shot_type", "shot_distance"]).copy()
+		filtered_data = filtered_data[filtered_data["season"].astype(str) == season].copy()
+		filtered_data["shot_distance"] = filtered_data["shot_distance"].round(0)
+		plot_data = filtered_data.groupby(["shot_distance", "shot_type"])["is_goal"].mean().to_frame().reset_index()
+		plot_data = plot_data[plot_data["shot_distance"] <= 100]
+		g = sns.relplot(data=plot_data, x="shot_distance", y="is_goal", col="shot_type",
+		                kind="line", linewidth=1, col_wrap=2, facet_kws={'sharey': False, 'sharex': False})
+		for ax in g.axes.flat:
+			ax.set_xlabel("shot_distance")
+			ax.set_ylabel("goal probability")
+			ax.set_xticks([0, 20, 40, 60, 80, 100])
+			ax.set_yticks([0.2, 0.4, 0.6, 0.8, 1.0])
+		plt.subplots_adjust(hspace=0.2)
+		g.fig.subplots_adjust(top=0.95)
+		g.fig.suptitle('Probability of Scoring Based on Shot Distance for Season 2017')
 		plt.show()
-		return fig
-
-	def distance_and_type_vs_goalv2(self, save_fig=True) -> plt.Figure:
-		"""
-		Depicts the relationship between shot-distance, shot_type and number of goals.
-		As described in part 5 question 3
-		TODO Work in progress.
-		:param save_fig: boolean to save the plot to SAVE_FIG_PATH
-		:return: a plt.Figure object instance
-		"""
-
-		fig = plt.figure(figsize=self.fig_size)
-		shot_type_dict = {}
-		for index, shot in enumerate(self.df['shot_type'].unique()):
-			shot_type_dict[shot] = index
-		self.df['shot_type'].replace(shot_type_dict, inplace=True)
-		sns.jointplot(data=self.df, x='shot_type', y='shot_distance', hue='is_goal')
 		if save_fig:
-			fig.savefig(os.path.join(SAVE_FIG_PATH, "distance_and_type_vs_goalv2.png"))
-		plt.show()
-		return fig
+			plt.savefig(os.path.join(SAVE_FIG_PATH, "Q5-3_distance_and_type_vs_goal.png"))
+		return g
+
+
 
 def main():
+	df = pd.read_csv(TIDY_DATA_PATH)
 	hockey_plotter = HockeyPlotter()
-	hockey_plotter.shot_type_histogram()
-	hockey_plotter.distance_vs_goal_chance()
-	hockey_plotter.distance_and_type_vs_goal()
-	hockey_plotter.distance_and_type_vs_goalv2()
+	# Plot Q5.1
+	hockey_plotter.shot_type_histogram(df)
+	# Plot Q5.2
+	hockey_plotter.distance_vs_goal_chance(df)
+	# Plot Q5.3
+	hockey_plotter.distance_and_type_vs_goal(df)
 
 if __name__ == "__main__":
 	main()
