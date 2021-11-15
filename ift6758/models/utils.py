a3a552ff04794648d1f892a1f7a7fc913c12f245
AxelBogos <35436065+AxelBogos@users.noreply.github.com>
2021-11-14 20:02:13 -0500

    Better comments and abstracted defaults

--- ift6758/models/utils.py
+++ ift6758/models/utils.py
@@ -4,6 +4,9 @@ from typing import List
 from sklearn.preprocessing import StandardScaler
 
 TIDY_DATA_PATH = os.path.join(os.path.dirname(__file__), '..', 'data', 'tidy_data.csv')
+# TODO double-check which seasons we should be using. Milestone2 description seems inconsistent with Milestone 1 data.
+DEFAULT_TRAIN_SEASONS = ['20162017', '20172018', '20182019', '20192020']
+DEFAULT_TEST_SEASONS = ['20202021']
 
 
 def load_data(features: List[str], train_val_seasons: List[str] = None, test_season: List[str] = None,
@@ -11,9 +14,8 @@ def load_data(features: List[str], train_val_seasons: List[str] = None, test_sea
     """
     Loads the dataset, drops all but the desired features and target var and returns the train_val_test split.
     :param features: List of features to be used as strings. Ex: ['shot_distance']
-    :param train_val_seasons: List of seasons to be used for the train & val sets. Default: ['20162017', '20172018',
-     '20182019', '20192020']
-    :param test_season: List of seasons to be used for the test set. Default: ['20202021']
+    :param train_val_seasons: List of seasons to be used for the train & val sets. Default: DEFAULT_TRAIN_SEASONS
+    :param test_season: List of seasons to be used for the test set. Default: DEFAULT_TEST_SEASONS
     :param train_val_ratio: Ratio of the train and val sets. Default: 0.2
     :param target: Target feature for classification/prediction.
     :param use_standard_scaler: Boolean to determine whether or not to scale features with SkLearn StandardScaler()
@@ -21,9 +23,9 @@ def load_data(features: List[str], train_val_seasons: List[str] = None, test_sea
     """
     assert features, 'Must provide training features'
     if train_val_seasons is None:
-        train_val_seasons = ['20162017', '20172018', '20182019', '20192020']
+        train_val_seasons = DEFAULT_TRAIN_SEASONS
     if test_season is None:
-        test_season = ['20202021']
+        test_season = DEFAULT_TEST_SEASONS
     df = pd.read_csv(TIDY_DATA_PATH)
 
     train_val = df[df['season'].astype(str).isin(train_val_seasons)]
