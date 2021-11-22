27e59f407cd1dad55b3acc5bab3682c848181062
AxelBogos <35436065+AxelBogos@users.noreply.github.com>
2021-11-22 14:07:07 -0500

    Simplify load_data()

--- ift6758/models/utils.py
+++ ift6758/models/utils.py
@@ -1,8 +1,6 @@
 import os.path
 import pandas as pd
 from typing import List
-import numpy as np
-from sklearn.preprocessing import StandardScaler
 
 TIDY_DATA_PATH = os.path.join(os.path.dirname(__file__), '..', 'data', 'tidy_data.csv')
 # TODO double-check which seasons we should be using. Milestone2 description seems inconsistent with Milestone 1 data.
@@ -10,20 +8,13 @@ DEFAULT_TRAIN_SEASONS = ['20162017', '20172018', '20182019', '20192020']
 DEFAULT_TEST_SEASONS = ['20202021']
 
 
-def load_data(features: List[str], train_val_seasons: List[str] = None, test_season: List[str] = None,
-              train_val_ratio: float = 0.2, target: str = 'is_goal', use_standard_scaler: bool = True,
-              return_as_dataframes: bool = True, drop_all_na: bool = True, convert_bool_to_int = True,
-              ) -> tuple:
+def load_data(features: List[str], train_val_seasons: List[str] = None, test_season: List[str] = None, target: str = 'is_goal') -> tuple:
     """
     Loads the dataset, drops all but the desired features and target var and returns the train_val_test split.
     :param features: List of features to be used as strings. Ex: ['shot_distance']
     :param train_val_seasons: List of seasons to be used for the train & val sets. Default: DEFAULT_TRAIN_SEASONS
     :param test_season: List of seasons to be used for the test set. Default: DEFAULT_TEST_SEASONS
-    :param train_val_ratio: Ratio of the train and val sets. Default: 0.2
     :param target: Target feature for classification/prediction.
-    :param use_standard_scaler: Boolean to determine whether or not to scale features with SkLearn StandardScaler()
-    :param return_as_dataframes: True to returns datasets as pd.DataFrame/Series, False to return as np.arrays
-    :param drop_all_na: True to drop all rows with a NAN feature. False to do no such processing
     :return: X_train, y_train, X_val, y_val, X_test, y_test as tuple
     """
     assert features, 'Must provide training features'
@@ -37,34 +28,10 @@ def load_data(features: List[str], train_val_seasons: List[str] = None, test_sea
     df[target] = df[target].astype(int)
 
     # Split train-val-test by seasons
-    train_val = df[df['season'].astype(str).isin(train_val_seasons)]
-    val = train_val.sample(frac=train_val_ratio, random_state=123)
-    train = train_val.drop(val.index)
+    train = df[df['season'].astype(str).isin(train_val_seasons)]
     test = df[df['season'].astype(str).isin(test_season)]
 
-    if drop_all_na:
-        train = train.dropna(subset=features)
-        val = val.dropna(subset=features)
-        test = test.dropna(subset=features)
-
     X_train, y_train = train.drop(train.columns.difference(features), axis=1), train[target]
-    X_val, y_val = val.drop(val.columns.difference(features), axis=1), val[target]
     X_test, y_test = test.drop(test.columns.difference(features), axis=1), test[target]
 
-    if use_standard_scaler:
-        scaler = StandardScaler()
-        X_train[X_train.select_dtypes(include=[np.number]).columns.values] = scaler.fit_transform(X_train[X_train.select_dtypes(include=[np.number]).columns.values])
-        X_val[X_val.select_dtypes(include=[np.number]).columns.values] = scaler.transform(X_val[X_val.select_dtypes(include=[np.number]).columns.values])
-        X_test[X_test.select_dtypes(include=[np.number]).columns.values] = scaler.transform(X_test[X_test.select_dtypes(include=[np.number]).columns.values])
-
-    if convert_bool_to_int:
-        X_train[X_train.select_dtypes(include=[bool]).columns.values] = X_train[X_train.select_dtypes(include=[bool]).columns.values].astype(int)
-        X_val[X_val.select_dtypes(include=[bool]).columns.values] = X_val[X_val.select_dtypes(include=[bool]).columns.values].astype(int)
-        X_test[X_test.select_dtypes(include=[bool]).columns.values] = X_test[X_test.select_dtypes(include=[bool]).columns.values].astype(int)
-
-    if return_as_dataframes:
-        X_train, y_train = pd.DataFrame(data=X_train, columns=features), pd.Series(y_train, name=target)
-        X_val, y_val = pd.DataFrame(data=X_val, columns=features), pd.Series(y_val, name=target)
-        X_test, y_test = pd.DataFrame(data=X_test, columns=features), pd.Series(y_test, name=target)
-
-    return X_train, y_train, X_val, y_val, X_test, y_test
+    return X_train, y_train, X_test, y_test
