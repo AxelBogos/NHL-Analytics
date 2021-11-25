fde191e0b9db53791d70ae17cfced7fe675a8a69
AxelBogos <35436065+AxelBogos@users.noreply.github.com>
2021-11-25 15:22:31 -0500

    fixes for now

--- ift6758/models/xgb/5_1.py
+++ ift6758/models/xgb/5_1.py
@@ -1,81 +1,60 @@
-import os.path
-from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.metrics import accuracy_score
-from scipy import stats
-from sklearn.metrics import roc_curve, auc, precision_recall_curve
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.calibration import CalibrationDisplay
 
 # import figure plot
 from create_figure import *
 
 # import utils
 import sys
+
 utils_path = os.path.abspath(os.path.join('..'))
 sys.path.append(utils_path)
-from utils import *
+from ift6758.models.utils import *
 import os
 from dotenv import load_dotenv
+import numpy as np
 load_dotenv()
 COMET_API_KEY = os.getenv('COMET_API_KEY')
 
-def xgb_basic_figure(model, feature):
 
+def train_basic_xgb(X_train, y_train, X_val):
+	model = XGBClassifier(use_label_encoder=False, eval_metric='error')
+	model.fit(X_train, y_train)
+	y_pred = model.predict_proba(X_val)[:, 1]
+	return y_pred
 
-    X_train, y_train, X_val, y_val, _, _ = load_data(
-                                                features = feature,
-                                                train_val_seasons = DEFAULT_TRAIN_SEASONS, 
-                                                test_season = DEFAULT_TEST_SEASONS,
-                                                train_val_ratio = 0.2, 
-                                                target = 'is_goal',
-                                                use_standard_scaler = False,
-                                                return_as_dataframes= False,
-                                                drop_all_na=  False
-    )
-    
-    model.fit(X_train, y_train)
-    
-    y_pred = model.predict_proba(X_val)[:,1]
-
-    return y_val, y_pred
-    
 
 def main():
-    '''
+	'''
     create 4 figures and save and /figures with each feature
     '''
-    y_val_vec = []
-    y_pred_vec = []
-    fig_name = ['5_1_dist', '5_1_angl', '5_1_dist_angl']
-    
-    model = XGBClassifier(use_label_encoder=False, eval_metric = 'error')
-    feature = ['shot_distance']
-    y_val, y_pred = xgb_basic_figure(model, feature)
-    
-    y_val_vec.append(y_val)
-    y_pred_vec.append(y_pred)
-    
-    feature = ['shot_angle']
-    y_val, y_pred = xgb_basic_figure(model, feature)
-    
-    y_val_vec.append(y_val)
-    y_pred_vec.append(y_pred)  
-    
-    feature = ['shot_distance', 'shot_angle']
-    y_val, y_pred = xgb_basic_figure(model, feature)
-    
-    y_val_vec.append(y_val)
-    y_pred_vec.append(y_pred)
-    
-    fig_roc_auc(y_val_vec, y_pred_vec, fig_name)
-    fig_cumulative_goal(y_val_vec, y_pred_vec, fig_name)
-    fig_goal_rate(y_val_vec, y_pred_vec, fig_name)
-    calibration_fig(y_val_vec, y_pred_vec, fig_name)
+
+	X_train, y_train, X_val, y_val, _, _ = load_data(
+		features=['shot_distance', 'shot_angle'],
+		train_val_seasons=DEFAULT_TRAIN_SEASONS,
+		test_season=DEFAULT_TEST_SEASONS,
+		train_val_ratio=0.2,
+		target='is_goal',
+		use_standard_scaler=True,
+		return_as_dataframes=True,
+		drop_all_na=False
+	)
+	y_pred_vec = []
+	FEATURES_LIST = [['shot_distance'], ['shot_angle'], ['shot_distance', 'shot_angle']]
+
+	# All combinations of features
+	for features in FEATURES_LIST:
+		sub_X_train = X_train[features]
+		sub_X_val = X_val[features]
+		y_pred = train_basic_xgb(sub_X_train, y_train, sub_X_val)
+		y_pred_vec.append(y_pred)
+
+	# Random Baseline
+	y_pred_vec.append(np.random.uniform(0, 1, size=y_val.shape[0]))
+
+	#fig_roc_auc(y_val, y_pred_vec)
+	fig_cumulative_goal(y_val, y_pred_vec)
+	# fig_goal_rate(y_val, y_pred_vec)
+	# calibration_fig(y_val, y_pred_vec)
+
 
 if __name__ == "__main__":
-    main()
+	main()
--- ift6758/models/xgb/create_figure.py
+++ ift6758/models/xgb/create_figure.py
@@ -1,4 +1,3 @@
-
 import os
 
 from xgboost import XGBClassifier
@@ -12,71 +11,72 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.calibration import CalibrationDisplay
+FIGURE_PATH = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'figures', 'milestone2', 'Q5')
+MODELS = ['Distance', 'Angle', 'Distance + Angle', 'Random']
 
-FIGURE_PATH = os.path.join(os.path.dirname(__file__), '..','..','..', 'figures')
-
-def fig_roc_auc(y_val, y_pred, fig_name) -> None:
-    '''
-    y_val: testing label
-    y_pred: probability of estimate x_test
-    '''
-    # plot ROC & AUC
-    fpr, tpr, _ = roc_curve(y_val, y_pred)
-    roc_auc = auc(fpr, tpr)
-    
-    #plt.figure()
-    plt.plot(
-        fpr,
-        tpr,
-        color="darkorange",
-        label="ROC curve (area = %0.2f)" % roc_auc,
-    )
+def fig_roc_auc(y_val, y_pred_vec) -> None:
+    fig = plt.figure(figsize=(10, 10))
+    for idx, y_pred in enumerate(y_pred_vec):
+        # Get FPR, TPR and AUC
+        fpr, tpr, _ = roc_curve(y_val, y_pred)
+        roc_auc = auc(fpr, tpr)
+        plt.plot(fpr, tpr, label= f"ROC ({MODELS[idx]}): area = {roc_auc:.3f})")
     plt.plot([0, 1], [0, 1], color="navy", linestyle="--")
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel("False Positive Rate")
     plt.ylabel("True Positive Rate")
-    plt.title("Receiver operating characteristic example")
+    plt.title("ROC Curves of Various Feature Selection")
     plt.legend(loc="lower right")
-    
-    fig_name = fig_name + '_roc_auc.png'
-    plt.savefig(os.path.join(FIGURE_PATH, fig_name))
+
+    fig_name = '5-1_roc_auc.png'
+    plt.show()
+    fig.savefig(os.path.join(FIGURE_PATH, fig_name))
     plt.close()
     return None
 
-# plot GOAL RATE
-def fig_cumulative_goal(y_val, y_pred, fig_name) -> None:
+
+def cumulative_goal_data(y_test, y_pred):
+    '''
+    y_test: testing label
+    y_pred: probability of estimate x_test
+    use ecdfplot in seaborn for estimate y_pred probability
+    '''
+    y_pred_percentile = 100 * stats.rankdata(y_pred, "min") / len(y_pred)
+    test_est = np.array([np.round(y_pred_percentile), y_test]).T
+    df_test_est = pd.DataFrame(test_est, columns=['model_per', 'is_goal'])
+
+    df_fil = df_test_est[df_test_est['is_goal'] == 1]
+    return df_fil
+
+def fig_cumulative_goal(y_val, y_pred_vec) -> None:
     '''
     y_val: testing label
     y_pred: probability of estimate x_test
     use ecdfplot in seaborn for estimate y_pred probability
     '''
-    y_pred_percentile = 100*stats.rankdata(y_pred, "min")/len(y_pred)
-    test_est = np.array([np.round(y_pred_percentile), y_val]).T
-    df_test_est = pd.DataFrame(test_est, columns = ['model_per', 'is_goal'])
-    
-    df_fil = df_test_est[df_test_est['is_goal'] == 1]
-    plt.figure()
-    ax = sns.ecdfplot(data=df_fil, x=100-df_fil.model_per)
-    yvals = ax.get_yticks()
-    #plt.yticks(np.arange(min(yvals), max(yvals)*1.05, 0.1))
-    plt.yticks(np.arange(0, 1.05, 0.1))
+    fig = plt.figure(figsize=(10, 10))
+    for idx, y_pred in enumerate(y_pred_vec):
+        cumulative_data = cumulative_goal_data(y_val,y_pred)
+        ax = sns.ecdfplot(data=cumulative_data, x=100 - cumulative_data.model_per, label=MODELS[idx])
 
-    plt.xticks(np.arange(0, 100*1.01, 10))
+    yvals = ax.get_yticks()
+    # plt.yticks(np.arange(min(yvals), max(yvals)*1.05, 0.1))
+    plt.yticks(np.arange(0, 1.05, 0.1))
+    plt.xticks(np.arange(0, 100 * 1.01, 10))
     xvals = ax.get_xticks()
     ax.set_xticklabels(100 - xvals.astype(np.int32))
-    
     yvals = ax.get_yticks()
     ax.set_yticklabels(['{:,.0%}'.format(y) for y in yvals])
     ax.set(xlabel='Shot probability model percentile')
     ax.set_title("Cumulative % of Goals")
-    
-    fig_name = fig_name + '_cumulative_goal.png'
-    plt.savefig(os.path.join(FIGURE_PATH, fig_name))
-
-
+    plt.legend(loc='lower right')
+    plt.show()
+    fig_name = '5-2_cumulative_goals.png'
+    fig.savefig(os.path.join(FIGURE_PATH,fig_name))
+    # experiment.log_figure(figure_name='cumulative_goal', figure=plt)
     plt.close()
-    return None
+
 
 def fig_goal_rate(y_val, y_pred, fig_name) -> None:
     '''
@@ -87,58 +87,56 @@ def fig_goal_rate(y_val, y_pred, fig_name) -> None:
     change xlable, ylabel of the figure
     '''
     # plot GOAL RATE
-    test_est = np.array([np.round(y_pred*100), y_val]).T
+    test_est = np.array([np.round(y_pred * 100), y_val]).T
     df_test_est = pd.DataFrame(test_est)
-    
+
     g = df_test_est.groupby(0)
-    
+
     # count goals.
     feature_mat = np.array(g.sum())
-            
+
     # count total of shots + goals
     group_count = np.array(g[[0]].count())
-    
-    
-    goal_percentate = feature_mat / group_count # goal / (goal + shot)
+
+    goal_percentate = feature_mat / group_count  # goal / (goal + shot)
     model_percentage = list(g.groups.keys())
 
     # convert model_percentage to percentile
-    model_percentile = 100*stats.rankdata(model_percentage, "min")/len(model_percentage)
-    
-    goal_rate = np.array([goal_percentate[:,0], model_percentile])
-    
-    df_test_est = pd.DataFrame(goal_rate[:,::-1].T, columns = ['goal_per', 'model_per'])
-    
+    model_percentile = 100 * stats.rankdata(model_percentage, "min") / len(model_percentage)
+
+    goal_rate = np.array([goal_percentate[:, 0], model_percentile])
+
+    df_test_est = pd.DataFrame(goal_rate[:, ::-1].T, columns=['goal_per', 'model_per'])
+
     xval = 100 - df_test_est.model_per
     plt.figure()
-    ax = sns.lineplot(x = xval, y = df_test_est.goal_per)
-    
+    ax = sns.lineplot(x=xval, y=df_test_est.goal_per)
+
     yvals = ax.get_yticks()
-    #plt.yticks(np.arange(min(yvals), max(yvals)*1.05, 0.1))
+    # plt.yticks(np.arange(min(yvals), max(yvals)*1.05, 0.1))
     plt.yticks(np.arange(0, 1.05, 0.1))
-        
-        
-    plt.xticks(np.arange(0, 100*1.01, 10))
-    
-    
+
+    plt.xticks(np.arange(0, 100 * 1.01, 10))
+
     ax.set(xlabel='Shot probability model percentile', ylabel="Goals/(Shots + Goals)")
-    
+
     ax.set(ylim=(0.05, 1.05))
     ax.set(xlim=(0, 110))
-    
+
     yvals = ax.get_yticks()
     ax.set_yticklabels(['{:,.0%}'.format(y) for y in yvals])
     xvals = ax.get_xticks()
-    
+
     ax.set_xticklabels(100 - xvals.astype(np.int32))
     ax.set_title("Goal Rate")
-    
+
     fig_name = fig_name + '_goal_rate.png'
     plt.savefig(os.path.join(FIGURE_PATH, fig_name))
 
     plt.close()
     return None
-    
+
+
 def calibration_fig(y_val, y_pred, fig_name) -> None:
     '''
     create calibration curve figures from y_val, y_pred database
