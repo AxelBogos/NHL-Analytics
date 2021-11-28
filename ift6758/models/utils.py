427be43687a46f076db924c11e20693f61bad7a9
AxelBogos <35436065+AxelBogos@users.noreply.github.com>
2021-11-28 13:39:47 -0500

    add template files

--- ift6758/models/utils.py
+++ ift6758/models/utils.py
@@ -11,7 +11,8 @@ DEFAULT_TEST_SEASONS = ['20202021']
 
 def load_data(features: List[str], train_val_seasons: List[str] = None, test_season: List[str] = None,
               train_val_ratio: float = 0.2, target: str = 'is_goal', use_standard_scaler: bool = True,
-              return_as_dataframes: bool = True, drop_all_na: bool = True) -> tuple:
+              return_as_numpy: bool = False, drop_all_na: bool = False, do_split_val: bool = True,
+              convert_bool_to_int=True, one_hot_encode_categoricals = False) -> tuple:
     """
     Loads the dataset, drops all but the desired features and target var and returns the train_val_test split.
     :param features: List of features to be used as strings. Ex: ['shot_distance']
@@ -20,8 +21,11 @@ def load_data(features: List[str], train_val_seasons: List[str] = None, test_sea
     :param train_val_ratio: Ratio of the train and val sets. Default: 0.2
     :param target: Target feature for classification/prediction.
     :param use_standard_scaler: Boolean to determine whether or not to scale features with SkLearn StandardScaler()
-    :param return_as_dataframes: True to returns datasets as pd.DataFrame/Series, False to return as np.arrays
+    :param return_as_numpy: True to returns datasets as an np.array, False to return as pd.DataFrame/Series
     :param drop_all_na: True to drop all rows with a NAN feature. False to do no such processing
+    :param do_split_val: True to split train into train-val.
+    :param convert_bool_to_int: If true, converts boolean columns to ints.
+    :param one_hot_encode_categoricals: If true, one-hot encods categorical columns.
     :return: X_train, y_train, X_val, y_val, X_test, y_test as tuple
     """
     assert features, 'Must provide training features'
@@ -31,33 +35,61 @@ def load_data(features: List[str], train_val_seasons: List[str] = None, test_sea
         test_season = DEFAULT_TEST_SEASONS
     df = pd.read_csv(TIDY_DATA_PATH)
 
+    if not do_split_val:
+        train_val_ratio = 0
+
     # Convert to numeric classes
     df[target] = df[target].astype(int)
 
     # Split train-val-test by seasons
-    train_val = df[df['season'].astype(str).isin(train_val_seasons)]
-    val = train_val.sample(frac=train_val_ratio, random_state=123)
-    train = train_val.drop(val.index)
+    train = df[df['season'].astype(str).isin(train_val_seasons)]
+    val = train.sample(frac=train_val_ratio, random_state=0)
+    train = train.drop(val.index)
     test = df[df['season'].astype(str).isin(test_season)]
 
+    # drop all NAN rows
     if drop_all_na:
         train = train.dropna(subset=features)
+        train = train.reset_index()
         val = val.dropna(subset=features)
+        val = val.reset_index()
         test = test.dropna(subset=features)
+        test = test.reset_index()
 
+    # Split X, y
     X_train, y_train = train.drop(train.columns.difference(features), axis=1), train[target]
     X_val, y_val = val.drop(val.columns.difference(features), axis=1), val[target]
     X_test, y_test = test.drop(test.columns.difference(features), axis=1), test[target]
 
+
+    # Scale numeric columns
     if use_standard_scaler:
+        NUMERIC_COLS = X_train.select_dtypes([np.number]).columns
         scaler = StandardScaler()
-        X_train = scaler.fit_transform(X_train)
-        X_val = scaler.transform(X_val)
-        X_test = scaler.transform(X_test)
+        X_train[NUMERIC_COLS] = scaler.fit_transform(X_train[NUMERIC_COLS])
+        if do_split_val:
+            X_val[NUMERIC_COLS] = scaler.transform(X_val[NUMERIC_COLS])
+        X_test[NUMERIC_COLS] = scaler.transform(X_test[NUMERIC_COLS])
 
-    if return_as_dataframes:
-        X_train, y_train = pd.DataFrame(data=X_train, columns=features), pd.Series(y_train, name=target)
-        X_val, y_val = pd.DataFrame(data=X_val, columns=features), pd.Series(y_val, name=target)
-        X_test, y_test = pd.DataFrame(data=X_test, columns=features), pd.Series(y_test, name=target)
+    # One-hot encode categorical columns
+    if one_hot_encode_categoricals:
+        CAT_COLS = X_train.select_dtypes(exclude=["number", "bool_"]).columns
+        X_train = pd.get_dummies(data=X_train, columns=CAT_COLS)
+        X_val = pd.get_dummies(data=X_val, columns=CAT_COLS)
+        X_test = pd.get_dummies(data=X_test, columns=CAT_COLS)
 
-    return X_train, y_train, X_val, y_val, X_test, y_test
+    if convert_bool_to_int:
+        BOOL_COLS = X_train.select_dtypes([bool]).columns
+        X_train[BOOL_COLS] = X_train[BOOL_COLS].astype(int)
+        X_val[BOOL_COLS] = X_val[BOOL_COLS].astype(int)
+        X_test[BOOL_COLS] = X_test[BOOL_COLS].astype(int)
+
+    if return_as_numpy:
+        X_train, y_train = X_train.to_numpy(), y_train.to_numpy()
+        X_val, y_val = X_val.to_numpy(), y_val.to_numpy()
+        X_test, y_test = X_test.to_numpy(), y_test.to_numpy()
+
+    if do_split_val:
+        return X_train, y_train, X_val, y_val, X_test, y_test
+    else:
+        return X_train, y_train, X_test, y_test
