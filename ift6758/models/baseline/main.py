eddf16becb4f5b3c06d2fe0157c7e758110638d2
MAGjagger <45465985+MAGjagger@users.noreply.github.com>
2021-11-29 11:08:58 -0500

    tidied up main file
    no longer calls 3 different files,
    streamlined graph creation
    added logging on comet split by model name

--- ift6758/models/baseline/main.py
+++ ift6758/models/baseline/main.py
@@ -1,79 +1,156 @@
 from comet_ml import Experiment
 from create_figure import *
-from ift6758.models import utils
-from q3_2 import *
-from q3_1 import *
-from q3_3 import *
+
+import os, sys
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+
+import pickle
+from cometconf import experiment
+import pandas as pd
+import numpy as np
+
+currentdir = os.path.dirname(os.path.realpath(__file__))
+parentdir = os.path.dirname(currentdir)
+sys.path.append(parentdir)
+
+import utils
+import create_figure
 
 
-if __name__ == "__main__":
+ 
+### setting x_train x_val only once to ensure same dimension and same removal of NaN
+feature_list = ['shot_distance', 'shot_angle']
+X_train, y_train, X_val, y_val, _, _ = utils.load_data(features=feature_list)
 
-    roc_data = []
-    tidy = pd.read_csv("../../data/tidy_data.csv", sep=",")
-    feature_list = ['shot_distance']
-    X_train, y_train, X_val, y_val, _, _ = utils.load_data(features=feature_list)
-    Q1_train_regression_evaluate(X_train, y_train, X_val, y_val)
-    model_r, y_valid_r, predictions_r_prob, y_prediction_r = Q2_regression_random(X_train, y_train, X_val, y_val)
-    model, y_valid, predictions_prob, y_prediction = Q2_regression_distance(X_train, y_train, X_val, y_val)
+with experiment.context_manager("Distance LogReg"): 
+    ### Q3.1 BASE MODEL WITH ONLY DISTANCE
+    X_train_dist = pd.DataFrame(X_train['shot_distance'])
+    X_valid_dist = pd.DataFrame(X_val['shot_distance'])
+    clf_dist = LogisticRegression()
+    clf_dist.fit(X_train_dist, y_train)
+    
+    predictions_distance = clf_dist.predict_proba(X_valid_dist)
+    predictions_labl_distance = clf_dist.predict(X_valid_dist)
+    accuracy_dist = clf_dist.score(X_valid_dist, y_val)
+     
+    # save model
+    file_name = "LogReg_dist_model.pkl"   
+    pickle.dump(clf_dist, open(file_name, "wb"))
+     
+    #compute metrics
+    f1 = f1_score(y_val, predictions_labl_distance)
+    accuracy = accuracy_score(y_val, predictions_labl_distance)
+    precision = precision_score(y_val, predictions_labl_distance)
+    recall = recall_score(y_val, predictions_labl_distance)
+    metrics = {"accuracy": accuracy, "f1": f1, "recall": recall, "precision": precision}
+    params = {
+         "model_type": 'Logistic Reg Distance',
+         "scaler": "standard scaler",
+         "param_grid": str(clf_dist.get_params()),
+    }
+    
+    experiment.log_parameters(params)
+    experiment.log_metrics(metrics)
+    experiment.add_tag('3-1 Dist LogReg')
+    experiment.log_model('3_1_dist_logreg__model', file_name)
+     
+ ### Q 3.2 Figures found in create_figure.py
+ 
+### Q3.3 models
+### RANDOM ###
+with experiment.context_manager("Random Baseline"): 
+    np.random.seed(123)
+    prediction_random = np.random.uniform(low=0.0, high=1.0, size=(len(X_val)))
+    prediction_labl_random = np.round(prediction_random)
+     
+    #compute metrics
+    f1 = f1_score(y_val, prediction_labl_random)
+    accuracy = accuracy_score(y_val, prediction_labl_random)
+    precision = precision_score(y_val, prediction_labl_random)
+    recall = recall_score(y_val, prediction_labl_random)
+    metrics = {"accuracy": accuracy, "f1": f1, "recall": recall, "precision": precision}
+    params = {
+         "model_type": 'Random Baseline',
+         "scaler": "None",
+         "param_grid": 'seed=123',
+     }
+    
+    experiment.log_parameters(params)
+    experiment.log_metrics(metrics)
+    experiment.add_tag('3-2 Random Baseline')
 
-    fpr_d, tpr_d, _ = metrics.roc_curve(y_valid, predictions_prob[::, 1])
-    auc_distance = metrics.roc_auc_score(y_valid, predictions_prob[::, 1])
-    roc_data.append([fpr_d, tpr_d, auc_distance, 'distance'])
+ 
+ 
+ ### ANGLE ###
+with experiment.context_manager("shot_angle_logreg"): 
+    X_train_angle = pd.DataFrame(X_train['shot_angle'])
+    X_val_angle = pd.DataFrame(X_val['shot_angle'])
+    clf_ang = LogisticRegression()
+    clf_ang.fit(X_train_angle, y_train)
+    predictions_angle = clf_ang.predict_proba(X_val_angle)
+    predictions_labl_angle = clf_ang.predict(X_val_angle)
+    accuracy_angle = clf_ang.score(X_val_angle, y_val)
+     
+    file_name = "LogReg_angle_model.pkl"   
+    pickle.dump(clf_ang, open(file_name, "wb"))
+     
+     #compute metrics
+    f1 = f1_score(y_val, predictions_labl_angle)
+    accuracy = accuracy_score(y_val, predictions_labl_angle)
+    precision = precision_score(y_val, predictions_labl_angle)
+    recall = recall_score(y_val, predictions_labl_angle)
+    metrics = {"accuracy": accuracy, "f1": f1, "recall": recall, "precision": precision}
+    params = {
+         "model_type": 'Logistic Reg Distance',
+         "scaler": "standard scaler",
+         "param_grid": str(clf_dist.get_params()),
+     }
+    
+    experiment.log_parameters(params)
+    experiment.log_metrics(metrics)
+    experiment.add_tag('3-3 Angle LogReg')
+    experiment.log_model('3_3_angle_logreg__model', file_name)
+     
+ 
+ ### DIST ANGLE ###
+with experiment.context_manager("shot_angle_dist_logreg"): 
+    clf_dist_ang = LogisticRegression()
+    clf_dist_ang.fit(X_train, y_train)
+    predictions_dist_angle = clf_dist_ang.predict_proba(X_val)
+    predictions_labl_dist_angle = clf_dist_ang.predict(X_val)
+    accuracy_dist_angle = clf_dist_ang.score(X_val, y_val)
+     
+    file_name = "LogReg_dist_angle_model.pkl"   
+    pickle.dump(clf_dist_ang, open(file_name, "wb"))
+       
+     #compute metrics
+    f1 = f1_score(y_val, predictions_labl_angle)
+    accuracy = accuracy_score(y_val, predictions_labl_angle)
+    precision = precision_score(y_val, predictions_labl_angle)
+    recall = recall_score(y_val, predictions_labl_angle)
+    metrics = {"accuracy": accuracy, "f1": f1, "recall": recall, "precision": precision}
+    params = {
+         "model_type": 'Logistic Reg Distance',
+         "scaler": "standard scaler",
+         "param_grid": str(clf_dist.get_params()),
+     }
+    
+    experiment.log_parameters(params)
+    experiment.log_metrics(metrics)
+    experiment.add_tag('3-3 Angle Dist LogReg')
+    experiment.log_model('3_3_angle_dist_logreg__model', file_name)
+ 
+data_graph = [predictions_distance[:,1,None],predictions_angle[:,1,None], predictions_dist_angle[:,1,None],prediction_random[:,None]]
 
-    fpr_r, tpr_r, _ = metrics.roc_curve(y_valid_r, predictions_r_prob)
-    auc_random = metrics.roc_auc_score(y_valid_r, predictions_r_prob)
-    roc_data.append([fpr_r, tpr_r, auc_random, 'random'])
+ 
+model_names = ['LogReg Distance', 'LogReg Angle', 'LogReg Angle + Distance', 'Random Baseline']
+fig_number = '3'
+ 
+#creating figures and saving them locally + on comet
+create_figure.fig_roc_auc(y_val, data_graph,fig_number,model_names=model_names,experiment=experiment)
+create_figure.calibration_fig(y_val, data_graph,fig_number,model_names=model_names,experiment=experiment)
+create_figure.fig_cumulative_goal(y_val, data_graph,fig_number,model_names=model_names,experiment=experiment)
+create_figure.fig_goal_rate(y_val, data_graph,fig_number,model_names=model_names,experiment=experiment)
 
-    feature_list = [ 'shot_angle']
-    X_train, y_train, X_val, y_val, _, _ = utils.load_data(features=feature_list)
-    model_angle, y_valid_angle, predictions_prob_angle, y_prediction_angle = Q3_regre_angle(X_train, y_train, X_val, y_val)
-
-    feature_list = ['shot_distance', 'shot_angle']
-    X_train, y_train, X_val, y_val, _, _ = utils.load_data(features=feature_list)
-    model_angle_dist, y_valid_angle_dist, predictions_prob_angle_dist, y_prediction_angle_dist = Q3_regression_angle_distance(X_train, y_train, X_val, y_val)
-
-    fpr_a, tpr_a, _ = metrics.roc_curve(y_valid_angle, predictions_prob_angle[::, 1])
-    auc_angle = metrics.roc_auc_score(y_valid_angle, predictions_prob_angle[::, 1])
-    roc_data.append([fpr_a, tpr_a, auc_angle, 'angle'])
-
-    fpr_a_d, tpr_a_d, _ = metrics.roc_curve(y_valid_angle_dist, predictions_prob_angle_dist[::, 1])
-    auc_angle_d = metrics.roc_auc_score(y_valid_angle_dist, predictions_prob_angle_dist[::, 1])
-    roc_data.append([fpr_a_d, tpr_a_d, auc_angle_d, 'angle_distance'])
-
-    plot_roc_curve(roc_data, 'AUC_data')
-
-    x_r, y_r = goal_rate_data(np.squeeze(y_valid_r), predictions_r_prob)
-    x_a, y_a = goal_rate_data(np.squeeze(y_valid_angle), np.squeeze(predictions_prob_angle[:, 1]))
-    x_d, y_d = goal_rate_data(np.squeeze(y_valid), np.squeeze(predictions_prob[:, 1]))
-    x_d_a, y_d_a = goal_rate_data(np.squeeze(y_valid_angle_dist), np.squeeze(predictions_prob_angle_dist[:, 1]))
-    list_goal_rate=[]
-    list_goal_rate.append([x_r, y_r, 'random'])
-    list_goal_rate.append([x_a, y_a, 'angle'])
-    list_goal_rate.append([x_d, y_d, 'distance'])
-    list_goal_rate.append([x_d_a, y_d_a, 'distance-angle'])
-    fig_goal_rate(list_goal_rate)
-
-
-    df_r = cumulative_goal_data(np.squeeze(y_valid_r), predictions_r_prob)
-    df_a = cumulative_goal_data(np.squeeze(y_valid_angle), np.squeeze(predictions_prob_angle[:, 1]))
-    df_d = cumulative_goal_data(np.squeeze(y_valid), np.squeeze(predictions_prob[:, 1]))
-    df_d_a = cumulative_goal_data(np.squeeze(y_valid_angle_dist), np.squeeze(predictions_prob_angle_dist[:, 1]))
-    list_cumul_data=[]
-    df_r.cat = 'Random'
-    df_a.cat = 'Angle'
-    df_d.cat = 'Distance'
-    df_d_a.cat = 'Distance-Angle'
-    list_cumul_data.append(df_r)
-    list_cumul_data.append(df_a)
-    list_cumul_data.append(df_d)
-    list_cumul_data.append(df_d_a)
-    fig_cumulative_goal(list_cumul_data)
-
-    list_calibration_data=[]
-
-    list_calibration_data.append([y_valid, predictions_prob[:, 1], 'Distance'])
-    list_calibration_data.append([y_valid_r, predictions_r_prob, 'Random'])
-    list_calibration_data.append([y_valid_angle, predictions_prob_angle[:, 1], 'Angle'])
-    list_calibration_data.append([y_valid_angle_dist, predictions_prob_angle_dist[:, 1], 'Distance-Angle'])
-
-    calibration_plot(list_calibration_data)
