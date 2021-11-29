1f877b632e35ac2b13dda997fc9f81c639c15e1f
MAGjagger <45465985+MAGjagger@users.noreply.github.com>
2021-11-29 09:11:32 -0500

    changed workspace to correct one

--- ift6758/models/baseline/cometconf.py
+++ ift6758/models/baseline/cometconf.py
@@ -3,6 +3,6 @@ from comet_ml import Experiment
 experiment = Experiment(
         api_key="YOUR-API-KEY",
         project_name="ift-6758-milestone-2",
-        workspace="houdasaadaoui",
+        workspace="axelbogos",
 
    )
