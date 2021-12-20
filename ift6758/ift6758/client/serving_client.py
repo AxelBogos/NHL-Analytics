c1b5e82c4d26d9ee59523bb1e3f58bcd757c1dec
MAGjagger <45465985+MAGjagger@users.noreply.github.com>
2021-12-20 08:11:59 -0500

    reupload ift6758/client
    in order to ensure no versioning error due to change in structure

--- ift6758/ift6758/client/serving_client.py
+++ ift6758/ift6758/client/serving_client.py
@@ -1,9 +1,20 @@
+import os
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+os.chdir('../')
+os.chdir('../')
+
 import json
 import requests
 import pandas as pd
 import logging
-from ift6758.ift6758.client.feature_lists import *
-from ift6758.ift6758.models.utils import load_data
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+print(os.curdir)
+from ift6758.client.feature_lists import *
+from ift6758.models.utils import load_live_data
+from sklearn.preprocessing import StandardScaler
+import pickle
 
 logger = logging.getLogger(__name__)
 
@@ -16,6 +27,7 @@ class ServingClient:
         if features is None:
             features = ["distance"]
         self.features = features
+        self.scaler = pickle.load(open('scaler.pkl','rb'))
 
         self.model_registries_to_file_name = {
             '6-lgbm': ('6-LGBM.pkl', feature_list_lgbm),
@@ -34,6 +46,8 @@ class ServingClient:
         Args:
             X (Dataframe): Input dataframe to submit to the prediction service.
         """
+        
+        X = load_live_data(features = feature_list,data = X, scaler =self.scaler)
         
         for column in self.features:
             if column not in X.columns:
@@ -68,7 +82,7 @@ class ServingClient:
         """
 
         assert model in self.model_registries_to_file_name.keys(), f'model name must be in ' \
-                                                                   f'{self.model_registries_to_file_name.keys()} '
+                                                                    f'{self.model_registries_to_file_name.keys()} '
         model_file_name = self.model_registries_to_file_name[model][0]
         self.features = self.model_registries_to_file_name[model][1]
         request = {'workspace': workspace, 'registry_name': model, 'model_name': model_file_name, 'version': version}
